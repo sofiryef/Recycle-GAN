6e3a85f9fe23c783ece92d08d2328c8fc2560a0e
sofiryef <sofiryef@gmail.com>
2019-01-02 20:36:26 +0200

    fix

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from options.test_options import TestOptions
 from data.data_loader import CreateDataLoader
 from models.models import create_model
 #from util.visualizer import Visualizer
-from util import html
+# from util import html
 
 opt = TestOptions().parse()
 opt.nThreads = 1   # test code only supports nThreads = 1
@@ -18,7 +18,7 @@ model = create_model(opt)
 #visualizer = Visualizer(opt)
 # create website
 web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.which_epoch))
-webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
+# webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
 # test
 for i, data in enumerate(dataset):
     if i >= opt.how_many:
@@ -30,4 +30,4 @@ for i, data in enumerate(dataset):
     print('%04d: process image... %s' % (i, img_path))
     #visualizer.save_images(webpage, visuals, img_path)
 
-webpage.save()
+# webpage.save()
